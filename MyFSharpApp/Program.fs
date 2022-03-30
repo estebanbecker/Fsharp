3baa67c34755be26a08cf631eefccedb38aff69e
Placard <31778398+estebanbecker@users.noreply.github.com>
2022-03-30 22:17:35 +0200

    New Programs

--- MyFSharpApp/Program.fs
+++ MyFSharpApp/Program.fs
@@ -1,33 +1,13 @@
-﻿module BasicFunctions = 
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-    /// You use 'let' to define a function. This one accepts an integer argument and returns an integer. 
-    /// Parentheses are optional for function arguments, except for when you use an explicit type annotation.
-    let sampleFunction1 x = x*x + 3
+open System
 
-    /// Apply the function, naming the function return result using 'let'. 
-    /// The variable type is inferred from the function return type.
-    let result1 = sampleFunction1 4573
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
 
-    // This line uses '%d' to print the result as an integer. This is type-safe.
-    // If 'result1' were not of type 'int', then the line would fail to compile.
-    printfn $"The result of squaring the integer 4573 and adding 3 is %d{result1}"
-
-    /// When needed, annotate the type of a parameter name using '(argument:type)'.  Parentheses are required.
-    let sampleFunction2 (x:int) = 2*x*x - x/5 + 3
-
-    let result2 = sampleFunction2 (7 + 4)
-    printfn $"The result of applying the 2nd sample function to (7 + 4) is %d{result2}"
-
-    /// Conditionals use if/then/elif/else.
-    ///
-    /// Note that F# uses white space indentation-aware syntax, similar to languages like Python.
-    let sampleFunction3 x = 
-        if x < 100.0 then 
-            2.0*x*x - x/5.0 + 3.0
-        else 
-            2.0*x*x + x/5.0 - 37.0
-
-    let result3 = sampleFunction3 (6.5 + 4.5)
-
-    // This line uses '%f' to print the result as a float.  As with '%d' above, this is type-safe.
-    printfn $"The result of applying the 3rd sample function to (6.5 + 4.5) is %f{result3}"
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
--- hello.fsx
+++ hello.fsx
@@ -1,1 +1,7 @@
-printfn "Hello World from F#"
+// Define a new function to print a name.
+let printGreeting name =
+    printfn $"Hello {name} from F#!"
+
+// Call your new function!
+printGreeting "Esteban"
+printGreeting "F#"
